a2f037e5cfe360d74d5789320bfb09efc9bb6faa
SuperMen <549514557@qq.com>
2019-08-30 16:16:09 +0800

    用户实体

---
+++ user/src/main/java/org/men/user/entity/User.java
@@ -0,0 +1,23 @@
+package org.men.user.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @ClassName User
+ * @Description 用户模块 分为浏览管理员 和 授权登录模块的人员信息
+ * @Author SuperMen
+ * Date 2019/8/30 16:13
+ * @Version 1.0
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+
+
+}
