2080665a8a1bdf795fe77c2d044a23685100fc1c
mfc <549514557@qq.com>
2019-09-16 21:49:47 +0800

    test ArrayList机制

---
+++ nacoscentre/src/main/java/org/men/nacoscentre/NacoscentreApplication.java
@@ -0,0 +1,14 @@
+package org.men.nacoscentre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class NacoscentreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NacoscentreApplication.class, args);
+    }
+
+}
